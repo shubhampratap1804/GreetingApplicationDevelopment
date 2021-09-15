d86bbfa0605f4d002e10f8ba26753573b5b8efe5
shubhampratap1804 <samsunggalaxygrand2pratap@gmail.com>
2021-09-15 23:19:21 +0530

    Created: greeting controller and http methods

---
+++ src/main/java/com/example/greeting/Greeting.java
@@ -0,0 +1,24 @@
+package com.example.greeting;
+
+public class Greeting {
+
+	private long id;
+	private String message;
+	
+	public Greeting(long incrementAndGet, String format) {
+		
+	}
+	
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getMessage() {
+		return message;
+	}
+	public void setMessage(String message) {
+		this.message = message;
+	}
+}
