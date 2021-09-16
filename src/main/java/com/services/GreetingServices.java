890f556ddd36e27439864cda50b226bf6de2f1e9
shubhampratap1804 <samsunggalaxygrand2pratap@gmail.com>
2021-09-16 21:01:32 +0530

    Added: ability to save greeting messages in repository layer

--- src/main/java/com/services/GreetingServices.java
+++ src/main/java/com/services/GreetingServices.java
@@ -9,4 +9,19 @@ import com.repository.GreetingRepository;
 @Service
 public class GreetingServices implements IGreetingServices {
 
+	private static final String template = "Hello, %s";
+	private final AtomicLong counter = new AtomicLong();
+	private GreetingRepository greetingRepository;
+
+	@Override
+	public Greeting addGreeting(User user) {
+		String message = String.format(template, user.toString().isEmpty() ? "HelloWorld" : user.toString());
+		return greetingRepository.save(new Greeting(counter.incrementAndGet(), message));
+	}
+
+	@Override
+	public Greeting getGreetingById(long id) {
+
+		return null;
+	}
 }
